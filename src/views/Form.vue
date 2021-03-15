d571f36befd93240aeab961e62043787d92fd2da
Dimas Satria <dimassatriia@gmail.com>
2021-03-15 21:53:55 +0700

    update permak

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -46,23 +46,16 @@
           <!-- /.row -->
         </div>
         <!-- /.container -->
-        <div class="one-row_bottom_divider pembatas" style="height: 180px">
-          <svg
-            fill="#ff546c"
-            xmlns="http://www.w3.org/2000/svg"
-            width="100%"
-            height="182"
-            viewBox="0 0 1920 182"
-            preserveAspectRatio="none"
-          >
-            <path
-              d="M1921.91,916.348c0.33,39.216-.34,79.431,0,118.642Q957.95,1035.5-6,1035V853c40.431,10.8,81,19.794,122.5,27.149,62.957,11.157,117.371,15.375,180.742,21.116,79.864,7.236,165.843,26.989,255.045,42.232,109.142,18.65,243.949,40.091,308.265,44.243,137.637,8.886,313.056-2.783,504.066-36.2,127.4-22.286,223.4-43.261,354.45-45.248A1569.414,1569.414,0,0,1,1921.91,916.348Z"
-              transform="translate(0 -853)"
-            ></path>
-          </svg>
-        </div>
+        <div
+          class="one-row_bottom_divider pembatas"
+          style="height: 180px"
+        ></div>
       </section>
-      <b-row class="w-100 py-5 p-md-5" align-v="center">
+      <b-row
+        style="background-color: #ff546c"
+        class="w-100 py-5 p-md-5"
+        align-v="center"
+      >
         <b-col
           class="px-5 pt-sm-3 pb-sm-3 pt-md-5 pb-md-5 text-center"
           cols="12"
@@ -71,7 +64,7 @@
         <b-col md="12">
           <b-card
             class="p-sm-1 p-md-5 mx-sm-1 mx-md-auto"
-            style="max-width: 700px"
+            style="max-width: 700px; border-radius: 3em"
           >
             <b-form @submit="onSubmit">
               <b-row v-for="f in form.segments" :key="f.name">
@@ -259,6 +252,19 @@
             </b-form>
           </b-card>
         </b-col>
+        <svg
+          fill="#fff"
+          xmlns="http://www.w3.org/2000/svg"
+          width="100%"
+          height="182"
+          viewBox="0 0 1920 182"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M1921.91,916.348c0.33,39.216-.34,79.431,0,118.642Q957.95,1035.5-6,1035V853c40.431,10.8,81,19.794,122.5,27.149,62.957,11.157,117.371,15.375,180.742,21.116,79.864,7.236,165.843,26.989,255.045,42.232,109.142,18.65,243.949,40.091,308.265,44.243,137.637,8.886,313.056-2.783,504.066-36.2,127.4-22.286,223.4-43.261,354.45-45.248A1569.414,1569.414,0,0,1,1921.91,916.348Z"
+            transform="translate(0 -853)"
+          ></path>
+        </svg>
         <b-col class="text-center mt-5 p-5" cols="12">
           <hr style="max-width: 800px; background-color: black" />
           <b
